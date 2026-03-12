6637337dabde4fca978be564c5e6497110b978f0
Your Name <you@example.com>
2026-03-12 18:44:02 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,13 +1,24 @@
 <?php
-$host = 'localhost';
-$dbname = 'mealplanner';
-$username = 'root';
-$password = '';
+
+$host = getenv("postgres.railway.internal");
+$port = getenv("5432");
+$dbname = getenv("PGDATABASE");
+$username = getenv("postgres");
+$password = getenv("ACuFZzZcArgzMTLzEeSQeQBMSxAntduD");
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $username,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+
 } catch(PDOException $e) {
+
     die("Connection failed: " . $e->getMessage());
+
 }
+
 ?>
