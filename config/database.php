3f5082a32ba86a9cb417a231a39abfff0d17564c
Your Name <you@example.com>
2026-03-12 17:13:20 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,11 @@
 <?php
-$host = 'db.fr-pari1.bengt.wasmernet.com';
-$port = '10272';
-$dbname = 'dbesjniLdeDSAvmQDjWj6dza';
-$username = '2d607d237b0180002a27dd4647d6';
-$password = '069b2d60-7d23-7c13-8000-50046a8d23d2';
+$host = 'localhost';
+$dbname = 'mealplanner';
+$username = 'root';
+$password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
