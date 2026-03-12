aa5e99362150a733210b5ef323abc9bb50e4e938
Your Name <you@example.com>
2026-03-12 17:49:43 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,19 @@
 <?php
-$host = 'localhost';
-$dbname = 'mealplanner';
-$username = 'root';
-$password = '';
+
+$host = "db.xhfrlzamhwntvlvlhzpm.supabase.co";
+$port = "5432";
+$dbname = "postgres";
+$username = "postgres";
+$password = "Qu74pqPtLRf,Wa@";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
+        $username,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
