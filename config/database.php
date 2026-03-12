8df3170e66348895dab1d815caf6963c43933619
Your Name <you@example.com>
2026-03-12 17:09:28 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'mealplanner';
-$username = 'root';
-$password = '';
+$host = 'db.fr-pari1.bengt.wasmernet.com';
+$dbname = 'dbesjniLdeDSAvmQDjWj6dza';
+$username = '2d607d237b0180002a27dd4647d6';
+$password = '069b2d60-7d23-7c13-8000-50046a8d23d2';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
