a7578d0027b7a8711a3f4b8ce2e5396c7a12c531
Your Name <you@example.com>
2026-03-13 16:23:10 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,8 +6,14 @@ $username = "postgres";
 $password = "Qu74pqPtLRf,Wa@";
 
 try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
+        $username,
+        $password
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
